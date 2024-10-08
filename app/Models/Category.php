409560b883d3c6fd09d650a49e314130c6c8456c
sob1rdev <20sobirjon05@gmail.com>
2024-10-08 17:51:17 +0500

    refactor: improve implementation for categories table

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
 {
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
+
+    public function products(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
